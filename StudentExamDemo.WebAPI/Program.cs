080abace521889ff54602ed4b260167ae34aa36a
Fahmin Gurbanli <gurbanlifahmin21@gmail.com>
2026-03-04 17:36:01 +0400

    webAPI_project_added

---
+++ StudentExamDemo.WebAPI/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using StudentExamDemo.Infrastructure;
+using StudentExamDemo.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Initialize the database and seed data
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+    await AppDbContextSeed.SeedAsync(db);
+    //await db.Database.MigrateAsync();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
