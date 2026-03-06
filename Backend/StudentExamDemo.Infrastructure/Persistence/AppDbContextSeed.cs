ec4c777e1a382b397811a766ad4f0587c8d81877
Fahmin Gurbanli <gurbanlifahmin21@gmail.com>
2026-03-06 09:57:30 +0400

    added Subject's feed data

--- Backend/StudentExamDemo.Infrastructure/Persistence/AppDbContextSeed.cs
+++ Backend/StudentExamDemo.Infrastructure/Persistence/AppDbContextSeed.cs
@@ -6,8 +6,19 @@ namespace StudentExamDemo.Infrastructure.Persistence
     {
         public static async Task SeedAsync(AppDbContext context)
         {
-            if (!context.Students.Any())
+            // Check if there are any students or subjects in the database
+            // If not, add some initial data for testing purposes
+            if (!context.Students.Any() && !context.Subjects.Any())
             {
+                context.Subjects.Add(new Subject
+                {
+                    Name = "Mathematics",
+                    Class = 11,
+                    Code = "MAT",
+                    TeacherName = "Əli",
+                    TeacherSurname = "Məmmədli"
+                });
+
                 context.Students.Add(new Student
                 {
                     Name = "Fahmin",
